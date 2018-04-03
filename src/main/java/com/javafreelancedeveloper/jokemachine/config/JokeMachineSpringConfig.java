0d778be7fe0928148b9769a03086f16771b649c2
peregrinetradersdev <yvette.quinby@peregrinetraders.com>
2018-04-03 10:05:52 +0200

    config

---
+++ src/main/java/com/javafreelancedeveloper/jokemachine/config/JokeMachineSpringConfig.java
@@ -0,0 +1,16 @@
+package com.javafreelancedeveloper.jokemachine.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JokeMachineSpringConfig {
+	
+	@Bean
+	public ChuckNorrisQuotes chuckNorrisQuotes() {
+		return new ChuckNorrisQuotes();
+	}
+
+}
--- src/main/java/com/javafreelancedeveloper/jokemachine/service/impl/ChuckNorrisJokeService.java
+++ src/main/java/com/javafreelancedeveloper/jokemachine/service/impl/ChuckNorrisJokeService.java
@@ -2,6 +2,7 @@ package com.javafreelancedeveloper.jokemachine.service.impl;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.javafreelancedeveloper.jokemachine.service.JokeService;
@@ -11,8 +12,9 @@ public class ChuckNorrisJokeService implements JokeService {
 
 	private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-	public ChuckNorrisJokeService() {
-		chuckNorrisQuotes = new ChuckNorrisQuotes();
+	@Autowired
+	public ChuckNorrisJokeService(ChuckNorrisQuotes chuckNorrisQuotes) {
+		this.chuckNorrisQuotes = chuckNorrisQuotes;
 	}
 
 	@Override

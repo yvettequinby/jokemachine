33525d7aaa89712de8dba89f19db11f7fa409620
peregrinetradersdev <yvette.quinby@peregrinetraders.com>
2018-04-03 10:27:13 +0200

    use java based config

--- src/main/java/com/javafreelancedeveloper/jokemachine/config/JokeMachineSpringConfig.java
+++ src/main/java/com/javafreelancedeveloper/jokemachine/config/JokeMachineSpringConfig.java
@@ -5,12 +5,19 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.javafreelancedeveloper.jokemachine.service.impl.ChuckNorrisJokeService;
+
 @Configuration
 public class JokeMachineSpringConfig {
 	
 	@Bean
 	public ChuckNorrisQuotes chuckNorrisQuotes() {
 		return new ChuckNorrisQuotes();
+	}
+	
+	@Bean
+	public ChuckNorrisJokeService chuckNorrisJokeService(ChuckNorrisQuotes chuckNorrisQuotes) {
+		return new ChuckNorrisJokeService(chuckNorrisQuotes);
 	}
 
 }
--- src/main/java/com/javafreelancedeveloper/jokemachine/service/impl/ChuckNorrisJokeService.java
+++ src/main/java/com/javafreelancedeveloper/jokemachine/service/impl/ChuckNorrisJokeService.java
@@ -2,17 +2,12 @@ package com.javafreelancedeveloper.jokemachine.service.impl;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.javafreelancedeveloper.jokemachine.service.JokeService;
 
-@Service
 public class ChuckNorrisJokeService implements JokeService {
 
 	private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-	@Autowired
 	public ChuckNorrisJokeService(ChuckNorrisQuotes chuckNorrisQuotes) {
 		this.chuckNorrisQuotes = chuckNorrisQuotes;
 	}

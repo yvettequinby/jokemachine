c018d3a605b91b211495391485ee80511b6c8345
peregrinetradersdev <yvette.quinby@peregrinetraders.com>
2018-04-03 10:17:18 +0200

    xml config

--- src/main/java/com/javafreelancedeveloper/jokemachine/JokeMachineApplication.java
+++ src/main/java/com/javafreelancedeveloper/jokemachine/JokeMachineApplication.java
@@ -2,8 +2,10 @@ package com.javafreelancedeveloper.jokemachine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:joke-machine-spring-config.xml")
 public class JokeMachineApplication {
 
 	public static void main(String[] args) {

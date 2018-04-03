4d1e5010f61946b89f38e4cdddb27e49f3826c08
peregrinetradersdev <yvette.quinby@peregrinetraders.com>
2018-04-03 06:48:09 +0200

    x

--- src/test/java/com/javafreelancedeveloper/jokemachine/JokeMachineApplicationTests.java
+++ src/test/java/com/javafreelancedeveloper/jokemachine/JokeMachineApplicationTests.java
@@ -1,4 +1,4 @@
-package com.javafreelancedeveloper.chucknorris;
+package com.javafreelancedeveloper.jokemachine;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ChucknorrisApplicationTests {
+public class JokeMachineApplicationTests {
 
 	@Test
 	public void contextLoads() {
